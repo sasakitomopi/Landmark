1f3ef725ae0c527291a23d9bdf0c8751cfe356c6
Tomohiro Sasaki <takatobi0326@icloud.com>
2021-07-18 14:35:53 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by user on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
